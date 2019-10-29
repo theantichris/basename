b5850b204a4f331782c24b90a0774c9f6e6d2d0b
Christopher Lamm <chris@theantichris.com>
2019-10-29 14:18:47 -0400

    feat: getting and returning basename

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
+// Basename removes directory components and a .suffix.
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func basename(s string) string {
+	// Discard last '/' and everything before.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+
+	// Preserve everything after the last '.'.
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+
+	return s
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		fmt.Println(basename(input.Text()))
+	}
+
+	// NOTE: ignoring potential errors from input.Err()
 }
